50686ec8ac41e42b5e11c24f7f01f6cf1e405534
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-15 04:02:05 +0530

    Improve validation messages and documentation in signup-user.dto.

--- src/users/dtos/signup-user.dto.ts
+++ src/users/dtos/signup-user.dto.ts
@@ -17,31 +17,30 @@ import {
 // At least one special character: (?=.*?[#?!@$%^&*-])
 // Minimum eight in length: .{8,} (with the anchors)
 // Remove the above groups as per the requirement to reduce the password strength.
-// For example, in the regex used below we just want 1 number and 1 special character. So we omitted
+// For example, in the regex used below we just want 1 digit and 1 special character. So we omitted
 // other groups.
 
 const MIN_USERNAME_LENGTH = 3
 
 export class SignUpDto {
-  //TODO: test if we need @IsNotEmpty
   @IsString()
   @MinLength(MIN_USERNAME_LENGTH)
   @Transform(({ value }: TransformFnParams) => value.trim()) // trim spaces
   username: string
 
-  //TODO: test if we need @IsNotEmpty
   @IsEmail()
   @Transform(({ value }: TransformFnParams) => value.trim())
   email: string
 
-  //TODO: test if we need @IsNotEmpty
   @IsString()
+  // Regex: Requires 1 digit and 1 special character.
   @Matches(/^(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-    message: 'Password must contain at least 1 digit and 1 special character.'
+    message: `Password must be 8 characters long and must contain at least 1 digit and 1 special character.`
   })
   password: string
 
   @IsString()
-  @IsNotEmpty() // required because we check the equality first.
+  // Required because we check the equality first.
+  @IsNotEmpty({ message: `Confirm Password must not be empty.` })
   confirmPassword: string
 }
