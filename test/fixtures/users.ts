aaff801bcfb930b3e76450e4e4a657c67f3e36f5
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-16 03:32:05 +0530

    Add test fixtures for testing the users.module.

---
+++ test/fixtures/users.ts
@@ -0,0 +1,13 @@
+export const userWithCorrectInfo = {
+  username: 'test1',
+  email: 'test1@test.com',
+  password: 'test123&',
+  confirmPassword: 'test123&'
+}
+
+export const userWithPasswordSevenChars = {
+  username: 'test2',
+  email: 'test2@test.com',
+  password: 't123&',
+  confirmPassword: 't123&'
+}
