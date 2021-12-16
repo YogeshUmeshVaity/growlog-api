71cea470eaf4025a86d495ae2bd27e88204af68d
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-16 19:40:38 +0530

    Improve tests in users.e2e-spec: - Add assertions for error messages in response. - Add test to validate minimum username length. - Add test for the validadation of email address.

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,18 +1,24 @@
 import { INestApplication } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
+import {
+  MIN_LENGTH_PASSWORD,
+  MIN_LENGTH_USERNAME
+} from '../src/users/dtos/signup-user.dto'
 import { AppModule } from './../src/app.module'
 import {
   userWithAlreadyExistingName,
   userWithConfirmPasswordNoMatch,
   userWithCorrectInfo,
+  userWithInvalidEmail,
   userWithPasswordSevenChars,
   userWithPasswordWithoutDigit,
-  userWithPasswordWithoutSpecialChars
+  userWithPasswordWithoutSpecialChars,
+  userWithUsernameTwoChars
 } from './fixtures/users.fixtures'
-import { clearDb } from './test.utils'
+import { clearDb, expectMessageFrom } from './test.utils'
 
-describe(`App (e2e)`, () => {
+describe(`UsersModule`, () => {
   let app: INestApplication
 
   beforeAll(async () => {
@@ -30,66 +36,96 @@ describe(`App (e2e)`, () => {
     await app.close()
   })
 
-  describe(`UsersModule`, () => {
-    describe(`sign-up route`, () => {
-      it(`should return a token when correct user info provided.`, async () => {
-        const response = await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithCorrectInfo)
-          .expect(201)
-      })
+  describe(`sign-up route`, () => {
+    it(`should return a token when correct user info provided.`, async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithCorrectInfo)
+        .expect(201)
+    })
 
-      it(`should throw when password less than 8 characters.`, async () => {
-        const response = await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithPasswordSevenChars)
-          .expect(400)
-      })
+    it(`should throw when username less than ${MIN_LENGTH_USERNAME} characters.`, async () => {
+      const response: request.Response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithUsernameTwoChars)
+        .expect(400)
+      expectMessageFrom(response).toBe(
+        `Username must be at least ${MIN_LENGTH_USERNAME} characters long.`
+      )
+    })
 
-      it(`should throw when password is without any special character.`, async () => {
-        const response = await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithPasswordWithoutSpecialChars)
-          .expect(400)
-      })
+    it(`should throw when email is invalid.`, async () => {
+      const response: request.Response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithInvalidEmail)
+        .expect(400)
+      expectMessageFrom(response).toBe('Please enter a valid email address.')
+    })
 
-      it(`should throw when password is without any digit.`, async () => {
-        const response = await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithPasswordWithoutDigit)
-          .expect(400)
-      })
+    it(`should throw when password less than ${MIN_LENGTH_PASSWORD} characters.`, async () => {
+      const response: request.Response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithPasswordSevenChars)
+        .expect(400)
+      expectMessageFrom(response).toBe(
+        `Password must be at least ${MIN_LENGTH_PASSWORD} characters long.`
+      )
+    })
 
-      it(`should throw when confirm password doesn't match with password.`, async () => {
-        const response = await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithConfirmPasswordNoMatch)
-          .expect(400)
-      })
+    it(`should throw when password is without any special character.`, async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithPasswordWithoutSpecialChars)
+        .expect(400)
+      expectMessageFrom(response).toBe(
+        'Password must contain at least 1 digit and 1 special character.'
+      )
+    })
 
-      it(`should throw when username already exists.`, async () => {
-        await clearDb()
-        await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithCorrectInfo)
-          .expect(201)
-        await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithAlreadyExistingName)
-          .expect(400)
-      })
+    it(`should throw when password is without any digit.`, async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithPasswordWithoutDigit)
+        .expect(400)
+      expectMessageFrom(response).toBe(
+        'Password must contain at least 1 digit and 1 special character.'
+      )
+    })
 
-      it(`should throw when email already exists.`, async () => {
-        await clearDb()
-        await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithCorrectInfo)
-          .expect(201)
-        await request(app.getHttpServer())
-          .post('/users/sign-up')
-          .send(userWithCorrectInfo)
-          .expect(400)
-      })
+    it(`should throw when confirm password doesn't match with password.`, async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithConfirmPasswordNoMatch)
+        .expect(400)
+      expectMessageFrom(response).toBe(
+        'Confirm Password must match with Password.'
+      )
+    })
+
+    it(`should throw when username already exists.`, async () => {
+      await clearDb()
+      await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithCorrectInfo)
+        .expect(201)
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithAlreadyExistingName)
+        .expect(400)
+      expectMessageFrom(response).toBe('Username already exists.')
+    })
+
+    it(`should throw when email already exists.`, async () => {
+      await clearDb()
+      await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithCorrectInfo)
+        .expect(201)
+      const response = await request(app.getHttpServer())
+        .post('/users/sign-up')
+        .send(userWithCorrectInfo)
+        .expect(400)
+      expectMessageFrom(response).toBe('Username already exists.')
     })
   })
 })
