a75e30c8426e0b8f833ed7c160315c86b6e06205
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-16 19:33:53 +0530

    Refactor signup-user.dto: - Add custom messages  for all fields. - Extract the regex in a separate variable. - Improve documentation.

--- src/users/dtos/signup-user.dto.ts
+++ src/users/dtos/signup-user.dto.ts
@@ -7,40 +7,53 @@ import {
   MinLength
 } from 'class-validator'
 
-// Note: It's not a good practice to have max length on passwords, usernames and emails:
-// https://stackoverflow.com/a/3797135/5925259
+/*
+ * Note: It's not a good practice to have max length on passwords, usernames and emails:
+ * https://stackoverflow.com/a/3797135/5925259
+ *
+ * Password Regex: ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$
+ * At least one upper case English letter: (?=.*?[A-Z])
+ * At least one lower case English letter: (?=.*?[a-z])
+ * At least one digit: (?=.*?[0-9])
+ * At least one special character: (?=.*?[#?!@$%^&*-])
+ * Minimum eight in length: .{8,} (with the anchors)
+ * For no minimum length: .*
+ * Remove the above groups as per the requirement to reduce the password strength.
+ * For example, in the regex used below we just want 1 digit and 1 special character. So we omitted
+ * other groups.
+ */
 
-// Password Regex: ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$
-// At least one upper case English letter: (?=.*?[A-Z])
-// At least one lower case English letter: (?=.*?[a-z])
-// At least one digit: (?=.*?[0-9])
-// At least one special character: (?=.*?[#?!@$%^&*-])
-// Minimum eight in length: .{8,} (with the anchors)
-// Remove the above groups as per the requirement to reduce the password strength.
-// For example, in the regex used below we just want 1 digit and 1 special character. So we omitted
-// other groups.
+export const MIN_LENGTH_USERNAME = 3
+export const MIN_LENGTH_PASSWORD = 8
 
-const MIN_USERNAME_LENGTH = 3
+/**
+ * Requires 1 digit and 1 special character. The dot means anything can go here and the star means
+ * at least 0 times so .* accepts any sequence of characters, including an empty string.
+ */
+const regexOneDigitOneSpecialChar = /^(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/
 
 export class SignUpDto {
   @IsString()
-  @MinLength(MIN_USERNAME_LENGTH)
+  @MinLength(MIN_LENGTH_USERNAME, {
+    message: `Username must be at least ${MIN_LENGTH_USERNAME} characters long.`
+  })
   @Transform(({ value }: TransformFnParams) => value.trim()) // trim spaces
   username: string
 
-  @IsEmail()
+  @IsEmail({}, { message: 'Please enter a valid email address.' })
   @Transform(({ value }: TransformFnParams) => value.trim())
   email: string
 
-  @IsString()
-  // Regex: Requires 1 digit and 1 special character.
-  @Matches(/^(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/, {
-    message: `Password must be 8 characters long and must contain at least 1 digit and 1 special character.`
+  @MinLength(MIN_LENGTH_PASSWORD, {
+    message: `Password must be at least ${MIN_LENGTH_PASSWORD} characters long.`
+  })
+  @Matches(regexOneDigitOneSpecialChar, {
+    message: 'Password must contain at least 1 digit and 1 special character.'
   })
   password: string
 
+  // Empty check is required because we check the equality first.
   @IsString()
-  // Required because we check the equality first.
   @IsNotEmpty({ message: `Confirm Password must not be empty.` })
   confirmPassword: string
 }
