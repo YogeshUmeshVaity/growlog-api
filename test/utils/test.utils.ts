8b28e925e79ce50e69aebdedf1778cc9a47ccd2c
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-18 06:19:18 +0530

    Document the tokenFrom() function in test.utils.

--- test/utils/test.utils.ts
+++ test/utils/test.utils.ts
@@ -1,6 +1,5 @@
 import { getConnection } from 'typeorm'
 import { Response } from 'supertest'
-import { response } from 'express'
 
 /**
  * Clears the database by synchronizing.
@@ -32,12 +31,45 @@ export const messageFrom = (response: Response) => {
   }
 }
 
+/**
+ * Decodes the payload of JWT without using the secret.
+ *
+ * On the server side:
+ * 1. JavaScript object with payload is converted to JSON string.
+ * 2. JSON string is converted to binary data.
+ *    Binary data cannot be sent in URL due to characters conflicts.
+ * 3. So, this binary data is encoded to Base64 string.
+ *    Base64 string still contains conflicting characters like '+', '/'.
+ * 4. So, this Base64 string is converted to Base64Url.
+ *    In Base64Url format, '+' and '/' are replaced by '-' and '_' respectively.
+ * 5. This Base64Url is what the content of the JWT, that is sent over the network.
+ *
+ * On the client side:
+ * That is, in this function, the above process is reversed to get the JavaScript object from JWT.
+ *
+ * @param response from Supertest.
+ * @returns JWT payload in JavaScript object.
+ */
 export const tokenFrom = (response: Response) => {
-  const token = response.body.token
-  const base64Url = token.split('.')[1] // token you get
+  const token: string = response.body.token
+
+  // Take the middle part of the JWT. This is what contains the payload, like userId.
+  const base64Url = token.split('.')[1]
+
+  // Convert it to base64 by replacing all the + with - and all the / with _
   const base64 = base64Url.replace('-', '+').replace('_', '/')
-  const decodedData = JSON.parse(
-    Buffer.from(base64, 'base64').toString('binary')
-  )
-  return decodedData
+
+  // Buffer in Node.js is used for reading and manipulating the streams of binary data.
+  // By specifying the encoding, we tell it that the data is of type base64, So it creates the
+  // appropriate representation of base64 into 8 bit sequence of binary bytes.
+  const buffer = Buffer.from(base64, 'base64')
+
+  // Convert the Buffer to a string. Because this payload is a JSON object(hence called JWT token),
+  // the string we get here is in JSON format. This process is called decoding the buffer.
+  const json = buffer.toString()
+
+  // Convert the JSON string into the JavaScript object.
+  const payload = JSON.parse(json)
+
+  return payload
 }
