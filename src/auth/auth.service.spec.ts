7f952ef4580448516c98159fef7b87ade7e4db08
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-19 20:14:31 +0530

    Setup unit tests in auth.service..spec.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,3 +1,5 @@
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthService } from './auth.service'
 
@@ -6,7 +8,30 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              switch (key) {
+                case 'JWT_SECRET':
+                  return 'test_secret'
+                case 'JWT_EXPIRY':
+                  return '1y'
+                default:
+                  return null
+              }
+            })
+          }
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockResolvedValue({})
+          }
+        }
+      ]
     }).compile()
 
     service = module.get<AuthService>(AuthService)
