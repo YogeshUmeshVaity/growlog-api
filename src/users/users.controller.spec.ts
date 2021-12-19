2d388f1f4445b1b34af6e21eb1b24c459a30bdc8
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-19 19:41:55 +0530

    Setup unit tests in users.controller.spec.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
 
 describe('UsersController', () => {
   let controller: UsersController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController]
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            signUp: jest.fn().mockResolvedValue({})
+          }
+        }
+      ]
     }).compile()
 
     controller = module.get<UsersController>(UsersController)
