2b34f8d2617a0321837dbd44526cbedffe97c69c
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-19 20:55:21 +0530

    Add function return types in public functions in users.repository.

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -7,15 +7,15 @@ import { User } from './user.entity'
 export class UsersRepository extends AbstractRepository<User> {
   private readonly logger = new Logger(UsersRepository.name)
 
-  async findByEmail(email: string) {
+  async findByEmail(email: string): Promise<User> {
     return await this.repository.findOne({ email })
   }
 
-  async findByName(username: string) {
+  async findByName(username: string): Promise<User> {
     return await this.repository.findOne({ username })
   }
 
-  async createAndSave(userInfo: SignUpDto) {
+  async createAndSave(userInfo: SignUpDto): Promise<User> {
     const user = this.repository.create({
       username: userInfo.username,
       email: userInfo.email,
