416b4bc1740b90fc9195b57b93f96f22ebadf764
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-22 21:15:25 +0530

    Add unit test in signup-user.dto to check if it throws error when the email is invalid.

--- src/users/dtos/signup-user.dto.spec.ts
+++ src/users/dtos/signup-user.dto.spec.ts
@@ -1,15 +1,24 @@
 import { plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator'
-import { userWithUsernameTwoChars } from '../../../test/users/fixtures/sign-up.fixtures'
+import {
+  userWithInvalidEmail,
+  userWithUsernameTwoChars
+} from '../../../test/users/fixtures/sign-up.fixtures'
 import { stringified } from '../../../test/utils/test.utils'
 import { MIN_LENGTH_USERNAME, SignUpDto } from './signup-user.dto'
 
 describe(`SignUpDto`, () => {
   it(`should throw when username less than ${MIN_LENGTH_USERNAME} characters.`, async () => {
     const signUpDto = plainToInstance(SignUpDto, userWithUsernameTwoChars)
-    const errors = await validate(signUpDto, /*{ skipMissingProperties: true }*/)
+    const errors = await validate(signUpDto /*{ skipMissingProperties: true }*/)
     expect(stringified(errors)).toContain(
       `Username must be at least ${MIN_LENGTH_USERNAME} characters long.`
     )
+  })
+
+  it(`should throw when email is invalid.`, async () => {
+    const signUpDto = plainToInstance(SignUpDto, userWithInvalidEmail)
+    const errors = await validate(signUpDto)
+    expect(stringified(errors)).toContain(`Please enter a valid email address.`)
   })
 })
