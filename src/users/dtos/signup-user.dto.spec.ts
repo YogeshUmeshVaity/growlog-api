484149b0664e54677432c95a0b98bc81db317646
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2021-12-22 21:17:48 +0530

    Add unit test in signup-user.dto to check if it throws error when password length is less than the minimum allowed.

--- src/users/dtos/signup-user.dto.spec.ts
+++ src/users/dtos/signup-user.dto.spec.ts
@@ -2,10 +2,11 @@ import { plainToInstance } from 'class-transformer'
 import { validate } from 'class-validator'
 import {
   userWithInvalidEmail,
+  userWithPasswordSevenChars,
   userWithUsernameTwoChars
 } from '../../../test/users/fixtures/sign-up.fixtures'
 import { stringified } from '../../../test/utils/test.utils'
-import { MIN_LENGTH_USERNAME, SignUpDto } from './signup-user.dto'
+import { MIN_LENGTH_PASSWORD, MIN_LENGTH_USERNAME, SignUpDto } from './signup-user.dto'
 
 describe(`SignUpDto`, () => {
   it(`should throw when username less than ${MIN_LENGTH_USERNAME} characters.`, async () => {
@@ -20,5 +21,13 @@ describe(`SignUpDto`, () => {
     const signUpDto = plainToInstance(SignUpDto, userWithInvalidEmail)
     const errors = await validate(signUpDto)
     expect(stringified(errors)).toContain(`Please enter a valid email address.`)
+  })
+
+  it(`should throw when password less than ${MIN_LENGTH_PASSWORD} characters.`, async () => {
+    const signUpDto = plainToInstance(SignUpDto, userWithPasswordSevenChars)
+    const errors = await validate(signUpDto)
+    expect(stringified(errors)).toContain(
+      `Password must be at least ${MIN_LENGTH_PASSWORD} characters long.`
+    )
   })
 })
