417c7ccb20cc81b2e3b9b6aaab58ae1934a06b37
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-01-05 02:24:14 +0530

    test(users.controller.spec): add unit test to ensure that signUp() returns a token

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import {
+  sampleToken,
+  userWithCorrectInfo as user
+} from '../../test/users/fixtures/sign-up.fixtures'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 describe('UsersController', () => {
-  let controller: UsersController
+  let usersController: UsersController
+  let usersService: UsersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,16 +17,25 @@ describe('UsersController', () => {
         {
           provide: UsersService,
           useValue: {
-            signUp: jest.fn().mockResolvedValue({})
+            signUp: jest.fn().mockResolvedValue(sampleToken)
           }
         }
       ]
     }).compile()
 
-    controller = module.get<UsersController>(UsersController)
+    usersController = module.get<UsersController>(UsersController)
+    usersService = module.get<UsersService>(UsersService)
   })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined()
+    expect(usersController).toBeDefined()
+  })
+
+  describe(`signUp`, () => {
+    it(`should return a token when correct user info provided.`, async () => {
+      const returnedToken = await usersController.signUp(user)
+      expect(returnedToken).toEqual(sampleToken)
+      expect(usersService.signUp).toBeCalledWith(user)
+    })
   })
 })
