5fd7033821ace6703254702945913b64fc044685
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-01-27 20:08:43 +0530

    docs(main.ts): update documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+/**
+ * Starting with the AppModule (root module), Nest assembles the dependency graph and begins the
+ * process of Dependency Injection and instantiates the classes needed to launch our application.
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
