f0d8ee6983906958f1471b37e30316d90bfcc583
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-04-09 19:37:54 +0530

    feat(authentication): add User type on the Request object in Express

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,15 @@
+import { User } from '../../users/user.entity'
+
+/**
+ * This file must be in the ./types/express directory of the project. The reason is that you can
+ * either create a directory named express or having a file named express.d.ts to tell typescript
+ * that you are aiming this specific module (which is installed through express type definitions).
+ * Augments existing definition of Request in Express.
+ */
+declare global {
+  declare namespace Express {
+    export interface Request {
+      user?: User
+    }
+  }
+}
