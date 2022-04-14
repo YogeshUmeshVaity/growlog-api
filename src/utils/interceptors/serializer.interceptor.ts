990c114ac65b2facbf951b594ad4330db1b28f49
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-04-14 16:44:20 +0530

    feat(serializer.interceptor): add serializer interceptor for hiding the sensitive properties

---
+++ src/utils/interceptors/serializer.interceptor.ts
@@ -0,0 +1,50 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors
+} from '@nestjs/common'
+import { plainToInstance } from 'class-transformer'
+import { map, Observable } from 'rxjs'
+
+/**
+ * Just makes sure that the object that we pass to the constructor is of some class. Some what
+ * better solution that passing 'any' to our Serialize decorator.
+ */
+interface ClassConstructor {
+  new (...args: any[]): unknown
+}
+
+/**
+ * Wraps up the SerializerInterceptor in a decorator. So we don't have to write a big line of code
+ * at the use-site. This class can be re-used for any DTO.
+ * @param dto to serialize.
+ * @returns the SerializeInterceptor.
+ */
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializerInterceptor(dto))
+}
+
+/**
+ * Excludes the properties from the response data according to a specified DTO.
+ * This interceptor applies to the outgoing response and not to the incoming request because
+ * we have defined our logic inside the handler object.
+ */
+export class SerializerInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  // The data variable here is the default object the Nest sends. e.g. User entity object.
+  // We convert that to our dto type.
+  // The map operator for RxJS maps the default Observable to our desired Observable.
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // The pipe() function takes observables as input and returns another observable.
+    // The previous observable stays unmodified.
+    return handler.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          // This makes sure only the properties with @Expose in the DTO will be included.
+          excludeExtraneousValues: true
+        })
+      })
+    )
+  }
+}
