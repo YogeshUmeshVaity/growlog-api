6ef1daf47001b7b22d2a90c3e6051da27c839009
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-04-22 20:36:24 +0530

    test(auth.service): write test for logIn()

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,43 +1,60 @@
 import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
+import { sampleToken } from '../../test/users/fixtures/sign-up.fixtures'
+import { User } from '../users/user.entity'
 import { AuthService } from './auth.service'
 
+function sampleUser() {
+  const user = new User()
+  user.id = '30ff0b89-7a43-4892-9ccc-86bb5f16e296'
+  user.username = 'abcabc123'
+  user.renewTokenInvalidator()
+  return user
+}
+
+const ConfigServiceMock = {
+  provide: ConfigService,
+  useValue: {
+    get: jest.fn((key: string) => {
+      switch (key) {
+        case 'JWT_SECRET':
+          return 'test_secret'
+        case 'JWT_EXPIRY':
+          return '1y'
+        default:
+          return null
+      }
+    })
+  }
+}
+
+const JwtServiceMock = {
+  provide: JwtService,
+  useValue: {
+    signAsync: jest.fn().mockResolvedValue(sampleToken)
+  }
+}
+
 describe('AuthService', () => {
-  let service: AuthService
+  let authService: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: ConfigService,
-          useValue: {
-            get: jest.fn((key: string) => {
-              switch (key) {
-                case 'JWT_SECRET':
-                  return 'test_secret'
-                case 'JWT_EXPIRY':
-                  return '1y'
-                default:
-                  return null
-              }
-            })
-          }
-        },
-        {
-          provide: JwtService,
-          useValue: {
-            signAsync: jest.fn().mockResolvedValue({})
-          }
-        }
-      ]
+      providers: [AuthService, ConfigServiceMock, JwtServiceMock]
     }).compile()
 
-    service = module.get<AuthService>(AuthService)
+    authService = module.get<AuthService>(AuthService)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(authService).toBeDefined()
+  })
+
+  describe(`logIn`, () => {
+    it(`should return a token.`, async () => {
+      const { token } = await authService.logIn(sampleUser())
+      expect(token).toEqual(sampleToken)
+    })
   })
 })
