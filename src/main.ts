4633d56fd449468505ee1f423e41b9c42b9cba8f
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-05-17 16:13:14 +0530

    fix(main): add the missing await keywords

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module'
 /**
  * Starting with the AppModule (root module), Nest assembles the dependency graph and begins the
  * process of Dependency Injection and instantiates the classes needed to launch our application.
+ *
+ * We use the catch() on the bootstrap() call to suppress the eslint rule no-floating-promises.
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
 }
-bootstrap()
+bootstrap().catch((error) => console.error(error))
