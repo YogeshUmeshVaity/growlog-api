2e3c50c6f0237029c22a14c257e07b5605768792
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-09-27 20:38:38 +0530

    feat(users): update users.module for the EmailService and PasswordRecoveryRepository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from '../auth/auth.module'
 import { AuthService } from '../auth/auth.service'
 import { GoogleAuthService } from '../auth/google-auth.service'
+import { EmailService } from './email.service'
+import { PasswordRecoveryRepository } from './password-recovery.repository'
 import { UsersController } from './users.controller'
 import { UsersRepository } from './users.repository'
 import { UsersService } from './users.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([UsersRepository, PasswordRecoveryRepository]),
+    AuthModule
+  ],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, GoogleAuthService]
+  providers: [UsersService, AuthService, GoogleAuthService, EmailService]
 })
 export class UsersModule {}
