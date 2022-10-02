9d8b2fbf5190c06724fc044e0d7643a9c9e7d6fe
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-10-02 11:04:50 +0530

    test(users): add tests for password-recovery.repository

---
+++ src/users/password-recovery.repository.spec.ts
@@ -0,0 +1,53 @@
+import { DataSource } from 'typeorm'
+import { sampleUser } from '../../test/users/fixtures/find-me.fixtures'
+import {
+  expiration,
+  recoveryCode
+} from '../../test/users/fixtures/recover-password.fixtures'
+import { createInMemoryDataSource } from '../../test/utils/in-memory-database'
+import { PasswordRecovery } from './password-recovery.entity'
+import { PasswordRecoveryRepository } from './password-recovery.repository'
+
+describe('PasswordRecoveryRepository', () => {
+  let passwordRecoveryRepository: PasswordRecoveryRepository
+  let dataSource: DataSource
+
+  beforeEach(async () => {
+    dataSource = createInMemoryDataSource()
+    await dataSource.initialize()
+    passwordRecoveryRepository = new PasswordRecoveryRepository(dataSource)
+  })
+
+  afterEach(async () => {
+    await dataSource.destroy()
+  })
+
+  it('should be defined.', () => {
+    expect(passwordRecoveryRepository).toBeDefined()
+  })
+
+  describe('createRecovery', () => {
+    it(`should create and save the password recovery in the database.`, async () => {
+      const passwordRecovery = await passwordRecoveryRepository.create(
+        recoveryCode,
+        sampleUser(),
+        expiration
+      )
+      expect(passwordRecovery).toBeInstanceOf(PasswordRecovery)
+    })
+  })
+
+  describe('DeleteRecovery', () => {
+    it(`should delete the password recovery in the database.`, async () => {
+      const createdRecovery = await passwordRecoveryRepository.create(
+        recoveryCode,
+        sampleUser(),
+        expiration
+      )
+      const deletedRecovery = await passwordRecoveryRepository.delete(
+        createdRecovery
+      )
+      expect(deletedRecovery).toEqual(createdRecovery)
+    })
+  })
+})
---
+++ test/users/fixtures/recover-password.fixtures.ts
@@ -0,0 +1,2 @@
+export const recoveryCode = '123'
+export const expiration = new Date()
