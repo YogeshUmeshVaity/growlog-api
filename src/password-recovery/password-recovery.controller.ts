b467e5f54c7ca173deb64d66aa3d83ae15d50fa4
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-10-22 12:31:29 +0530

    feat(password-recovery): add route for validating recovery code

--- src/password-recovery/password-recovery.controller.ts
+++ src/password-recovery/password-recovery.controller.ts
@@ -1,22 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { RecoverPasswordDto } from './dtos/recover-password.dto'
+import { ValidateCodeDto } from './dtos/validate-recovery-code.dto'
 import { PasswordRecoveryService } from './password-recovery.service'
 
+/**
+ * Handles the routes related to the account recovery of the User.
+ */
 @Controller('recovery')
 export class PasswordRecoveryController {
   constructor(
     private readonly passwordRecoveryService: PasswordRecoveryService
   ) {}
 
+  /**
+   * Sends a recovery email with a recovery code to the user's email address.
+   * @param recoverDto is a validated email address.
+   */
   @Post('recover-password')
-  recoverPassword(@Body() recoverDto: RecoverPasswordDto) {
-    return this.passwordRecoveryService.recover(recoverDto.email)
+  async recoverPassword(@Body() recoverDto: RecoverPasswordDto) {
+    return await this.passwordRecoveryService.recover(recoverDto.email)
   }
 
-  // @Get(':recoveryCode')
-  // validateRecoveryLink(@Param() recoveryCode: string) {
-  //   throw new NotImplementedException()
-  // }
+  /**
+   * Checks whether the recovery code provided by the user is valid. This is used before showing
+   * the password reset screen.
+   * @param validateCodeDto contains recovery code and username.
+   * @returns the same recovery code and username.
+   */
+  @Get('validate-code')
+  async validateCode(@Body() validateCodeDto: ValidateCodeDto) {
+    return await this.passwordRecoveryService.validateCode(validateCodeDto)
+  }
 
   // @Post('change-password')
   // changePassword(
