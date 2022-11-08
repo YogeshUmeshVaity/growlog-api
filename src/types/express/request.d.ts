0cd08a0b9f68053946cf726f4ae1b446f20198bb
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2022-11-08 18:47:12 +0530

    docs(types): update documentation

--- src/types/express/request.d.ts
+++ src/types/express/request.d.ts
@@ -2,6 +2,7 @@ import { User } from '../../users/user.entity'
 
 /**
  * TODO: Move types folder to utils, so we only have feature modules in the src directory.
+ *
  * This file must be in the ./types/express directory of the project. The reason is that you can
  * either create a directory named express or having a file named express.d.ts to tell typescript
  * that you are aiming this specific module (which is installed through express type definitions).
